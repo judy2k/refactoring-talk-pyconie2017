a0d23df6d6197d9b4b40f2e30e250d3cc97fab4e
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-10-21 15:19:20 +0100

    Simplify property example.

--- code/fields_to_properties/after.py
+++ code/fields_to_properties/after.py
@@ -1,30 +1,6 @@
-from functools import lru_cache
+import time
 
-
-@lru_cache()
-def fib(index):
-    if index < 2:
-        return 1
-    return fib(index-2) + fib(index-1)
-
-
-class FibonacciResult(object):
-    def __init__(self, index):
-        self._index = index
-        self._value = None
-
+class EncryptionParameters:
     @property
-    def value(self):
-        print("Generate a value on demand!")
-        if self._value is None:
-            self._value = fib(self._index)
-        return self._value
-
-
-print("Constructing... ", end="", flush=True)
-five = FibonacciResult(5)
-print("done")
-print(five.value)
-# Constructing... done
-# Generate a value on demand!
-# 8
+    def salt(self):
+        return time.time()
